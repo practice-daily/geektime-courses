88253f7a4979c46205032611a0accbdd0dcdd229
squirrel <squirrel@squirreldeMBP.lan>
2019-08-08 23:39:36 +0800

    typescript: 高级类型--索引类型

--- ts/src/advanced.ts
+++ ts/src/advanced.ts
@@ -29,11 +29,11 @@ class Cat1 implements CatInterface {
 enum Master { Boy, Girl }
 function getPet(master: Master) {
   let pet = master === Master.Boy ? new Dog1() : new Cat1()
-  pet.eat() // 只能访问联合类型的公有成员
+  pet.eat() // 只能访问联合类型的共有成员
   return pet
 }
 
-// 可区分的联合类型：有公共属性
+// 可辨识联合类型：有公共属性
 interface Square {
   kind: 'square'
   size: number
@@ -57,8 +57,38 @@ function area(s: Shape) {
     case 'circle':
       return Math.PI * s.radius ** 2
     default:
+      // 使用never类型，编译器用它来进行完整性检查
       return ((e: never) => {throw new Error(e)})(s)
   }
 }
 console.log(area({kind: 'square', size: 100}))
 console.log(area({kind: 'circle', radius: 1}))
+
+// 索引类型
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+function getValues1(obj: any, keys: string[]) {
+  return keys.map(k => obj[k])
+}
+console.log(getValues1(obj, ['a', 'b']))
+console.log(getValues1(obj, ['d', 'e'])) // ts不会检查
+
+// keyof T，索引类型查询操作符：对于任何类型T，keyof T的结果为T上已知的公共属性名的联合。
+interface Obj {
+  a: number,
+  b: string
+}
+let aa: keyof Obj
+
+// T[K]，索引访问操作符
+let aaa: Obj['a']
+
+// 改造目的：keys的成员是obj的属性
+function getValues2<T, K extends keyof T>(obj: T, keys: K[]): T[K][] {
+  return keys.map(k => obj[k])
+}
+console.log(getValues2(obj, ['a', 'b']))
+// console.log(getValues2(obj, ['d', 'e'])) // ts会检查进行报错提示
